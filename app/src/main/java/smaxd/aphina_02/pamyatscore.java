51cf74f1d4d567757a08e11b4474a3fbe5058460
Maxim Solovyev <SMaximD@gmail.com>
2017-05-13 14:38:57 +0700

    Сброс рекордов

--- app/src/main/java/smaxd/aphina_02/pamyatscore.java
+++ app/src/main/java/smaxd/aphina_02/pamyatscore.java
@@ -1,17 +1,22 @@
 package smaxd.aphina_02;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class pamyatscore extends AppCompatActivity {
+public class pamyatscore extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
+      //  getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pamyatscore);
+        Button ResetBtn = (Button) findViewById(R.id.button2);
+        ResetBtn.setOnClickListener(this);
 
         TextView scoreView = (TextView) findViewById(R.id.high_scores_list);
         SharedPreferences scorePrefs = getSharedPreferences(
@@ -23,5 +28,28 @@ public class pamyatscore extends AppCompatActivity {
             scoreBuild.append(score + "\n");
         }
         scoreView.setText(scoreBuild.toString());
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        //
+        if (view.getId() == R.id.button2) {
+            Reset();
+        }
+    }
+
+    public void Reset() {
+        TextView scoreView = (TextView) findViewById(R.id.high_scores_list);
+        SharedPreferences scorePrefs = getSharedPreferences(
+                pamyat.PAM_PREFS, 0);
+        String[] savedScores = scorePrefs.getString("highScores", "").split(
+                "\\|");
+        StringBuilder scoreBuild = new StringBuilder("");
+        for (String score : savedScores) {
+            scoreBuild.append("" + "\n");
+        }
+        scoreView.setText(scoreBuild.toString());
+        getSharedPreferences(pamyat.PAM_PREFS,0).edit().clear().commit();
     }
 }
