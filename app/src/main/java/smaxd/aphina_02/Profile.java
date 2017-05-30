52745ae1e54f3d35577377c8d595598766c0fe4d
Maxim Solovyev <SMaximD@gmail.com>
2017-05-30 15:38:16 +0700

    1

--- app/src/main/java/smaxd/aphina_02/Profile.java
+++ app/src/main/java/smaxd/aphina_02/Profile.java
@@ -2,10 +2,12 @@ package smaxd.aphina_02;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.Games;
 
@@ -16,16 +18,57 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+
+    private GoogleApiClient mGoogleApiClient;
 
     private static final int REQUEST_ACHIEVEMENTS = 5;
    // private GoogleApiClient mGoogleApiClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+    }
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+        //  findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+       //  Attempt to reconnect
+          mGoogleApiClient.connect();
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //  if (mResolvingConnectionFailure) {
+        // already resolving
+        return;
+    }
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
     public void ToSettings(View view) {
@@ -53,9 +96,12 @@ public class Profile extends AppCompatActivity {
         startActivity(intent);
     }
     public void ToAchievements(View view) {
+
+        if (mGoogleApiClient.isConnected()){
         startActivityForResult(Games.Achievements.getAchievementsIntent(MainActivity.mGoogleApiClient),
                 REQUEST_ACHIEVEMENTS);
+        }
+        }
     }
 
-    }
 
